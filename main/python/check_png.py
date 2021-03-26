9fb82b8b524fd3f60e3ddc85c0c52eca0ac0c30d
henkel <anonymer.schokoholiker@gmail.com>
2021-03-26 14:39:41 +0100

    Formatierung angepasst

--- main/python/check_png.py
+++ main/python/check_png.py
@@ -5,39 +5,42 @@ MAX_DIFF = 300
 
 
 def check_png(dateipfad, png_byte_order='big'):
-    print("Analysiere {0}".format(dateipfad))
+    print(f"Analysiere {dateipfad}")
     tatsaechliche_groesse_in_bytes = os.path.getsize(dateipfad)
+
     with open(dateipfad, 'rb') as byte_reader:
         if not (ist_png_datei(byte_reader)):
             return False
+
         erwartete_groesse_in_bytes = get_erwartete_groesse(byte_reader, png_byte_order)
         differenz = tatsaechliche_groesse_in_bytes - erwartete_groesse_in_bytes
+
         if differenz > MAX_DIFF:
             print(f"Die errechnete Größe beträgt etwa {erwartete_groesse_in_bytes / 1000}KB.")
             print(f"Die Datei ist tatsächlich {tatsaechliche_groesse_in_bytes / 1000}KB groß.")
             print(f"Die Differenz beträgt {differenz / 1000}KB.")
             print(f"Das sind {100 / erwartete_groesse_in_bytes * differenz}% mehr als errechnet.")
             return False
+
         return True
 
 
 def ist_png_datei(byte_reader):
-    # Image file header. The first eight bytes are always the same for an image with file format png
     erste_acht_byte = bytes_lesen(byte_reader, 8)
     erwartete_erste_acht_byte = b'\x89\x50\x4E\x47\x0D\x0A\x1A\x0A'
 
     if erste_acht_byte == erwartete_erste_acht_byte:
         return True
     else:
-        print("Die ersten acht Byte sind {0}".format(erste_acht_byte))
-        print("Die ersten acht Byte sollten {0} sein".format(erwartete_erste_acht_byte))
+        print(f"Die ersten acht Byte sind {erste_acht_byte}")
+        print(f"Die ersten acht Byte sollten {erwartete_erste_acht_byte} sein")
         return False
 
 
 def get_erwartete_groesse(byte_reader, png_byte_order):
     ihdr_werte = get_ihdr_werte(byte_reader, png_byte_order)
     bits = berechne_bits(ihdr_werte)
-    bytes = berechne_bytes(bits)
+    bytes = bits / 8
     return bytes
 
 
@@ -53,10 +56,12 @@ def get_ihdr_werte(byte_reader, png_byte_order):
         hoehe = int.from_bytes(bytes_lesen(byte_reader, 4), byteorder=png_byte_order)
         bit_tiefe = int.from_bytes(bytes_lesen(byte_reader, 1), byteorder=png_byte_order)
         farb_kanaele = get_anzahl_farb_kananaele(int.from_bytes(bytes_lesen(byte_reader, 1), byteorder=png_byte_order))
+
         kompressions_methode = bytes_lesen(byte_reader, 1)
         filter_methode = bytes_lesen(byte_reader, 1)
         interlace_methode = bytes_lesen(byte_reader, 1)
         crc = bytes_lesen(byte_reader, 4)
+
         return breite, hoehe, bit_tiefe, farb_kanaele
     else:
         print("Fehler beim Lesen des Image Header (IHDR)")
@@ -84,10 +89,6 @@ def bytes_lesen(byte_reader, anzahl_bytes):
 def berechne_bits(ihdr_werte):
     breite, hoehe, bit_tiefe, farb_kanaele = ihdr_werte
     return breite * hoehe * bit_tiefe * farb_kanaele
-
-
-def berechne_bytes(bits):
-    return bits / 8
 
 
 if __name__ == '__main__':
